f9058f181bf38b97fb53996cf0cdac89ffdf393c
schultzd584 <68926393+schultzd584@users.noreply.github.com>
2022-03-29 16:27:43 -0500

    made find intersection method

---
+++ line_intersection.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def findIntersection(poly, checker):
+    try:
+        m_list = [[poly[0], poly[1]], [checker[0], checker[1]]]
+        A = np.array(m_list)
+        inv_A = np.linalg.inv(A)
+        print(inv_A)
+        B = np.array([poly[2], checker[2]])
+        X = np.linalg.inv(A).dot(B)
+        print(X)
+        return X
+    except:
+        return
+    
+
+
+poly = [4, 3, 20]
+checker = [-5, 9, 26]
+
+findIntersection(poly, checker)
