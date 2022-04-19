e94564e8cf537b3a7a8a6ce37afde811ddd413e8
schultzd584 <68926393+schultzd584@users.noreply.github.com>
2022-04-19 15:27:30 -0500

    fixed lamens way and added benchmarking methods

--- algorithm3.py
+++ algorithm3.py
@@ -1,5 +1,6 @@
 import random
-
+import time
+from tracemalloc import start
 
 def find_min(k, arr, tally):
     pickRand(arr)
@@ -59,25 +60,49 @@ def sort_around_pivot(pivot, arr):
 
 
 def boringWay(arr, k):
-    quickSort(arr)
+    quick_sort(arr, 0, len(arr) - 1)
+    print(arr)
     return arr[k - 1]
 
 
-def quickSort(arr, low, high):
-    if(low < high):
-        pivot = partition(arr, low, high)
-        quickSort(arr, low, pivot - 1)
-        quickSort(arr, pivot + 1, high)
+def quick_sort(array, low, high):
+  if low < high:
+ 
+    # Find pivot element such that
+    # element smaller than pivot are on the left
+    # element greater than pivot are on the right
+    pi = partition(array, low, high)
+ 
+    # Recursive call on the left of pivot
+    quick_sort(array, low, pi - 1)
+ 
+    # Recursive call on the right of pivot
+    quick_sort(array, pi + 1, high)
 
-def partition(arr, low, high):
-    pivot = arr[high]
-    i = low - 1
-    for j in range(low, high - 1):
-        if(arr[j] < pivot):
-            i = i + 1
-            (arr[i], arr[j]) = (arr[j], arr[i])
-    (arr[i + 1], arr[high]) = (arr[high], arr[i + 1])
-    return i + 1
+def partition(array, low, high):
+ 
+  # Choose the rightmost element as pivot
+  pivot = array[high]
+ 
+  # Pointer for greater element
+  i = low - 1
+ 
+  # Traverse through all elements
+  # compare each element with pivot
+  for j in range(low, high):
+    if array[j] <= pivot:
+      # If element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+ 
+      # Swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+ 
+  # Swap the pivot element with the greater element specified by i
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+ 
+  # Return the position from where partition is done
+  return i + 1
 
 
 #simple tests
@@ -85,5 +110,38 @@ test1 = [5,4,3,2,1]
 testK1 = 2
 test2 = [9,3,5,2,8]
 testK2 = 1
+print(boringWay(test2, testK2))
 
 
+#make benchmarks
+def create_reverse(n):
+    arr = []
+    index = 0
+    for x in range(n, 0, -1):
+        arr[index] = x
+        index += 1
+    return arr
+
+def create_sorted(n):
+    arr = []
+    for x in range(0, n):
+        arr[x] = x
+    return arr
+
+def create_random(n):
+    arr = []
+    for x in range(0, n):
+        arr[x] = random.randint(0, n)
+    return arr
+
+def benchmark(arr, k, method):
+    if method:
+        start_time = time.perf_counter()
+        boringWay(arr, k)
+        end_time = time.perf_counter()
+        return end_time - start_time
+    else:
+        start_time = time.perf_counter()
+        find_min(k, arr, 0)
+        end_time = time.perf_counter()
+        return end_time - start_time
