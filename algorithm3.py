7c103ead3cc16def7b18641c61f66ab33ab3e89f
mikeysalgado <100546773+mikeysalgado@users.noreply.github.com>
2022-04-19 16:24:11 -0500

    update

--- algorithm3.py
+++ algorithm3.py
@@ -1,33 +1,37 @@
 import random
 import time
-from tracemalloc import start
+import numpy as np
 
-def find_min(k, arr, tally):
-    pickRand(arr)
+def find_min(k, arr, start, end):
+    if len(arr) >= 1:
+        if start >= end:
+            print("here" + str(arr[start]) + str(start))
+            return arr[start]
+        pickRand(arr, start, end)
 
+        pivot = sort_around_pivot(arr[0], arr)
+        if pivot == k - 1:
+            print("there")
+            return arr[pivot]
 
-    pivot = sort_around_pivot(arr[0], arr)
+        if k - 1 < pivot:
+            return find_min(k, arr, 0, pivot - 1)
 
-    if pivot == k - 1 - tally:
-        return arr[pivot]
-
-    if k - 1 < pivot:
-        find_min(k, arr[0 : pivot], tally)
-
-    else:
-        tally += pivot + 1
-        find_min(k, arr[pivot + 1 : ], tally)
+        else:
+            return find_min(k, arr, pivot + 1, end - 1)
 
 
 '''
 function chooses a random pivot position and swaps index 0 with it
 '''
-def pickRand(arr):
-    randomIndex = random.randint(0, len(arr) - 1)
+
+
+def pickRand(arr, start, end):
+    randomIndex = random.randint(start, end)
     arr[0], arr[randomIndex] = arr[randomIndex], arr[0]
+    # print(arr)
 
     return
-
 
 
 '''
@@ -36,32 +40,34 @@ to the left and elements greater than on the right
 code from 
 https://www.geeksforgeeks.org/quicksort-using-random-pivoting/
 '''
+
+
 def sort_around_pivot(pivot, arr):
     start = 0
     stop = len(arr) - 1
-    pivot = start # pivot
-     
+    pivot = start  # pivot
+
     # a variable to memorize where the
     i = start + 1
-     
+
     # partition in the array starts from.
     for j in range(start + 1, stop + 1):
-         
+
         # if the current element is smaller
         # or equal to pivot, shift it to the
         # left side of the partition.
         if arr[j] <= arr[pivot]:
-            arr[i] , arr[j] = arr[j] , arr[i]
+            arr[i], arr[j] = arr[j], arr[i]
             i = i + 1
-    arr[pivot] , arr[i - 1] =\
-            arr[i - 1] , arr[pivot]
+    arr[pivot], arr[i - 1] = \
+        arr[i - 1], arr[pivot]
     pivot = i - 1
-    return (pivot)
+    return pivot
 
 
 def boringWay(arr, k):
     quick_sort(arr, 0, len(arr) - 1)
-    print(arr)
+    # print(arr)
     return arr[k - 1]
 
 '''
@@ -110,14 +116,14 @@ def partition(array, low, high):
   return i + 1
 
 
-#simple tests
-test1 = [5,4,3,2,1]
+# simple tests
+test1 = [5, 4, 3, 2, 1]
 testK1 = 2
-test2 = [9,3,5,2,8]
+test2 = [9, 3, 5, 2, 8]
 testK2 = 1
 
 
-#make benchmarks
+# make benchmarks
 def create_reverse(n):
     arr = []
     index = 0
@@ -126,17 +132,21 @@ def create_reverse(n):
         index += 1
     return arr
 
+
 def create_sorted(n):
     arr = []
     for x in range(0, n):
         arr[x] = x
     return arr
 
+
 def create_random(n):
-    arr = []
-    for x in range(0, n):
-        arr[x] = random.randint(0, n)
+    arr = random.sample(range(0, n), n)
+    # arr = []
+    # for x in range(0, n):
+    #     arr[x] = random.randint(0, n)
     return arr
+
 
 def benchmark(arr, k, method):
     if method:
@@ -146,6 +156,11 @@ def benchmark(arr, k, method):
         return end_time - start_time
     else:
         start_time = time.perf_counter()
-        find_min(k, arr, 0)
+        print(find_min(k, arr, 0, len(arr) - 1))
         end_time = time.perf_counter()
         return end_time - start_time
+
+
+arr = create_random(10000)
+print(np.sort(arr))
+benchmark(arr, 15, False)
